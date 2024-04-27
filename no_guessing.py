b8a3c94346762eccc6224c6f0f3ae76cf3e7b715
BhanuPratap13 <167957524+BhanuPratap13@users.noreply.github.com>
2024-04-28 00:00:00 +0530

    Update no_guessing.py

--- no_guessing.py
+++ no_guessing.py
@@ -1,34 +1,11 @@
 import random
-
-def number_guessing_game():
-    small = int(input("Enter smaller number of the range: "))
-    big = int(input("Enter bigger number of the range: "))
-
-    if small >= big:
-        print("wrong range try again")
-        return
-
-    secret_number = random.randint(small, big)
-    attempts = 0
-
-    print(f"\nGuess a number between {small} and {big}.")
-
-    while True:
-        user_guess = int(input("Enter your guess: "))
-        attempts += 1
-
-        if user_guess < secret_number:
-            print("Too low! Try again.")
-        elif user_guess > secret_number:
-            print("Too high! Try again.")
-        else:
-            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
-            break
-
-    play_again = input("Do you want to play again? (yes/no): ").lower()
-    if play_again == 'yes':
-        number_guessing_game()
-    else:
-        print("Thanks for playing!")
-
-number_guessing_game()
+number = random.randint(1,100)
+guess = 0
+while guess != number:
+    guess = int(input("Enter Guess: "))
+    if (guess<number): 
+        print("Guess higher!")
+    elif (guess>number): 
+        print("Guess lower!")
+    else: 
+        print("You won!")
